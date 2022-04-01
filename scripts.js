94aeb324b5822d0b7a2d5ef137fb7cb890c1dfca
Amorim <mtsgamorim@gmail.com>
2022-04-01 15:28:57 -0300

    botoes acendendo

---
+++ scripts.js
@@ -0,0 +1,52 @@
+function botaoComida(elemento) {
+    const marcado = document.querySelector(".selecionado1");
+    if (marcado !== null) {
+        marcado.classList.remove("selecionado1");
+    }
+    elemento.classList.add("selecionado1");
+
+    const observarBebida = document.querySelector(".selecionado2");
+    const observarSobremesa = document.querySelector(".selecionado3");
+    if (observarBebida !== null && observarSobremesa !== null){
+        const antigoBotao = document.querySelector(".sem-selecao");
+        const novoBotao = document.querySelector(".com-selecao");
+        antigoBotao.classList.add("desliga");
+        novoBotao.classList.remove("desliga");
+    }
+
+} 
+
+
+function botaoBebida(elemento) {
+    const marcado = document.querySelector(".selecionado2");
+    if (marcado !== null) {
+        marcado.classList.remove("selecionado2");
+    }
+    elemento.classList.add("selecionado2");
+   
+    const observarComida = document.querySelector(".selecionado1");
+    const observarSobremesa = document.querySelector(".selecionado3");
+    if (observarComida !== null && observarSobremesa !== null){
+        const antigoBotao = document.querySelector(".sem-selecao");
+        const novoBotao = document.querySelector(".com-selecao");
+        antigoBotao.classList.add("desliga");
+        novoBotao.classList.remove("desliga");
+    }
+} 
+
+function botaoSobremesa(elemento) {
+    const marcado = document.querySelector(".selecionado3");
+    if (marcado !== null) {
+        marcado.classList.remove("selecionado3");
+    }
+    elemento.classList.add("selecionado3");
+
+    const observarBebida = document.querySelector(".selecionado2");
+    const observarComida = document.querySelector(".selecionado1");
+    if (observarBebida !== null && observarComida !== null){
+        const antigoBotao = document.querySelector(".sem-selecao");
+        const novoBotao = document.querySelector(".com-selecao");
+        antigoBotao.classList.add("desliga");
+        novoBotao.classList.remove("desliga");
+    }
+} 
